dffcd08b01e8b984b4d5eea6b13550e8c32c2d78
Dokky1337 <fredrik.dokken@hotmail.com>
2026-01-24 20:00:45 +0100

    Fix link for What'aBomb in the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
         <ul className={`nav-links ${isMenuOpen ? 'nav-open' : ''}`}>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="https://whatabomb.com" target="_blank" rel="noopener noreferrer">What'aBomb</a></li>
+          <li><a href="https://whatabomb.vivaladokky.online" target="_blank" rel="noopener noreferrer">What'aBomb</a></li>
           <li className="mobile-only">
             <a href="https://www.linkedin.com/in/dokkenfredrik/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           </li>
